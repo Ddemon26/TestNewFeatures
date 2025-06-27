2d1685e1442f09756e944c8cfcbde555c363c0d6
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-06-27 03:24:11 -0700

    Remove SpanExample and clean up unused usings
    Deleted SpanExample.cs and its meta file. Also removed unused using directives from TestRefTypes.cs and UI/TestNewFeaturesUI.cs to clean up the codebase.

--- SpanExample.cs
+++
@@ -1,5 +0,0 @@
-﻿#nullable enable
-using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using UnityEngine;
--- TestRefTypes.cs
+++ TestRefTypes.cs
@@ -1,8 +1,5 @@
 #nullable enable
-using System;
 using System.Runtime.InteropServices;
-using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace System.Runtime.CompilerServices {
     [AttributeUsage( AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false )]
